5d890f5d190c4dc5efb57f4ea35289db8c27b234
takashi <kanaya@a-works.asia>
2023-02-03 03:17:47 +0000

    Update 10 files

--- src/index.js
+++ src/index.js
@@ -6,9 +6,29 @@
     const inputText = document.getElementById("addText").value;
     document.getElementById("addText").value = "";
 
+    // liタグ生成
+    const li = document.createElement("li");
+    console.log(li);
+
     //div生成
     const div = document.createElement("div");
+    div.className = "list-raw";
     console.log(div);
+
+    // p生成
+    const p = document.createElement("p");
+    p.innerText = inputText;
+    console.log(p);
+
+    //divタグの子要素に各要素を設定
+    div.appendChild(p);
+    console.log(div);
+
+    li.appendChild(div);
+    console.log(li);
+
+    //未完了のリストに追加する
+    document.getElementById("incomplete-list").appendChild(li);
   };
 
   document
